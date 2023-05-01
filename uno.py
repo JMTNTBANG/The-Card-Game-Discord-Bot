2e6aaadce0f89b48906aa9717de8b086ba6fd6a7
James Miller <jamesedmiller8@gmail.com>
2023-05-01 00:55:35 -0500

    Added Deck Generation

--- uno.py
+++ uno.py
@@ -21,3 +21,32 @@ class Game:
             self.card = card
 
     def __init__(self):
+        def gen_deck():
+            deck = []                                                           # Creates Deck List
+
+            for number in range(10):                                            # Loops Through Each Number
+                for color in range(5):                                              # Loops Through Each Color
+                    if color > 0:                                                       # If Color isn't Wild
+                        deck.append(self.Card(color, number))                               # Add Card to Deck
+                        print(f"C:{color} N:{number}")
+                        if number != 0:                                                     # If Card isn't 0
+                            deck.append(self.Card(color, number))                               # Add Another Card
+                            print(f"C:{color} N:{number}")
+
+            for card in range(5):                                               # Loops Through Each Card Type
+                for color in range(5):                                              # Loops Through Each Color
+                    if color == 0 and card == 4:                                        # If Card is Wild Draw
+                        for i in range(4):                                                  #
+                            deck.append(self.Card(color, 10, card))                         # Adds Card 4 Times
+                            print(f"C:{color} T:{card}")
+                    elif card == 3:                                                     # Else If Type is Wild
+                        for i in range(4):  # Loops 4 Times                                 #
+                            deck.append(self.Card(color, 10, card))                         # Adds Card 4 Times
+                            print(f"C:{color} T:{card}")
+                    elif card != 3:                                                     # Else If Type isn't Wild
+                        for i in range(2):  # Loops Twice                                   #
+                            deck.append(self.Card(color, 10, card))                         # Adds Card Twice
+                            print(f"C:{color} T:{card}")
+            return deck
+
+        self.deck = gen_deck()
